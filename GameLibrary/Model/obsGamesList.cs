4ef0113991161ac35402eff62e7ad8d227164bb4
Matias Rømer <Matias Rmer@MATIAS-NB>
2016-11-22 10:00:58 +0100

    it works.. for now

--- GameLibrary/Model/obsGamesList.cs
+++ GameLibrary/Model/obsGamesList.cs
@@ -12,7 +12,7 @@ namespace GameLibrary.Model
         public obsGamesList() :base()
         {
             this.Add(new Game() { title = "Counter strike:GO", pris = 85.0, dato = new DateTime (2012,8,21), comment = "fast paced FPS game. 5/5, stars if you love FPS this will suit your needs" });
-            this.Add(new Game() { title = "The Witcher 3", pris = 189.0, dato = new DateTime (2015,5,19), comment = "Single Player RPG. great story/slaying monster game 5/5 stars. "  });
+            this.Add(new Game() { title = "The Witcher 3", pris = 280.0, dato = new DateTime (2015,5,19), comment = "Single Player RPG. great story/slaying monster game 5/5 stars. "  });
             this.Add(new Game() { title = "Fallout 4", pris = 349.0, dato = new DateTime (2015,11,10), comment = "Single player RPG sorymode game/Slaying Monster. 4/5 stars" });
 
         }
